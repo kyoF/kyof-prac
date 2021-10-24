02388b73ed873a7d61a48958faee05f13032382e
ekusoyk <fujiki.kyosuke@gmail.com>
2021-10-24 13:01:26 +0900

    JSの基礎5

--- js/script.js
+++ js/script.js
@@ -1,8 +1,30 @@
 // alert("My name is Kyosuke!");
-let admin, name;
-name = "John";
-admin = name;
-alert(admin);
+// let admin, name;
+// name = "John";
+// admin = name;
+// alert(admin);
 
-const ourPlanetName = "Earth";
-const visitUser = "kyosuke";
+// const ourPlanetName = "Earth";
+// const visitUser = "kyosuke";
+
+function test() {
+  // alert("a" / 0);
+  let name = "kyosuke";
+  alert(`My name is ${name}`);
+  // alert(typeof undefined);
+  // alert(typeof 0);
+  // alert(typeof 10n);
+  // alert(typeof true);
+  // alert(typeof "foo");
+  // alert(typeof Symbol("id"));
+  // alert(typeof Math);
+  // alert(typeof null);
+  // alert(typeof test);
+  // let name = "Ilya";
+
+  alert(`hello ${1}`);
+
+  alert(`hello ${"name"}`);
+
+  alert(`hello ${name}`);
+}
